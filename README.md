cf11908f9c39ee98b063d1ca33770179f34a3652
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2021-11-23 14:18:44 +0100

    initial osmocom boilerplate source tree
    Related: SYS#5599
    Depends: I0a46b147ec6a76d909df28136cfd2b764b2c75ea (libosmocore)
    Change-Id: I4352dd8738a1a9de6ba2fc250ee8eef69c65ff1e

---
+++ README.md
@@ -0,0 +1,41 @@
+libosmo-pfcp - PFCP protocol encoding and decoding, and generic PFCP endpoint implementation
+============================================================================================
+
+Homepage
+--------
+
+The official homepage of the project is
+https://osmocom.org/projects/libosmo-pfcp
+
+GIT Repository
+--------------
+
+You can clone from the official libosmo-pfcp.git repository using
+
+	git clone git://git.osmocom.org/libosmo-pfcp.git
+
+To submit patches, see "Contributing" below.
+
+Mailing List
+------------
+
+Discussions related to libosmo-pfcp are happening on the
+osmocom-net-gprs@lists.osmocom.org mailing list, please see
+https://lists.osmocom.org/postorius/lists/osmocom-net-gprs@lists.osmocom.org/
+for subscription options and the list archive.
+
+Please observe the [Osmocom Mailing List
+Rules](https://osmocom.org/projects/cellular-infrastructure/wiki/Mailing_List_Rules)
+when posting.
+
+Contributing
+------------
+
+Our coding standards are described at
+https://osmocom.org/projects/cellular-infrastructure/wiki/Coding_standards
+
+Submit patches at https://gerrit.osmocom.org/
+See also https://osmocom.org/projects/cellular-infrastructure/wiki/Gerrit
+
+The current patch queue for libosmo-pfcp can be seen at
+https://gerrit.osmocom.org/#/q/project:libosmo-pfcp+status:open
---
+++ contrib/jenkins.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# jenkins build helper script for libosmo-pfcp.  This is how we build on jenkins.osmocom.org
+#
+# environment variables:
+# * WITH_MANUALS: build manual PDFs if set to "1"
+# * PUBLISH: upload manuals after building if set to "1" (ignored without WITH_MANUALS = "1")
+#
+
+if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then
+	echo "Error: We need to have scripts/osmo-deps.sh from http://git.osmocom.org/osmo-ci/ in PATH !"
+	exit 2
+fi
+
+
+set -ex
+
+base="$PWD"
+deps="$base/deps"
+inst="$deps/install"
+export deps inst
+
+osmo-clean-workspace.sh
+
+mkdir "$deps" || true
+
+verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
+
+export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$inst/lib"
+export PATH="$inst/bin:$PATH"
+
+osmo-build-dep.sh libosmocore "" --disable-doxygen
+
+# Additional configure options and depends
+CONFIG=""
+if [ "$WITH_MANUALS" = "1" ]; then
+	CONFIG="--enable-manuals"
+fi
+
+set +x
+echo
+echo
+echo
+echo " =============================== libosmo-pfcp ==============================="
+echo
+set -x
+
+cd "$base"
+autoreconf --install --force
+./configure --enable-sanitize $CONFIG
+$MAKE $PARALLEL_MAKE
+LD_LIBRARY_PATH="$inst/lib" $MAKE check \
+  || cat-testlogs.sh
+LD_LIBRARY_PATH="$inst/lib" \
+  DISTCHECK_CONFIGURE_FLAGS="$CONFIG" \
+  $MAKE $PARALLEL_MAKE distcheck \
+  || cat-testlogs.sh
+
+$MAKE $PARALLEL_MAKE maintainer-clean
+osmo-clean-workspace.sh
